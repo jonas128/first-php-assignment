6d63c99b0fbd87e5d362b8cc446353eb536f19f1
h-emma <emmah92@hotmail.com>
2021-10-20 20:12:47 +0200

    Made a new function, does not work yet

--- functions.php
+++ functions.php
@@ -2,8 +2,19 @@
 
 declare(strict_types=1);
 
-function getRandomIndex(array $anyArray)
+function getRandomIndex(array $anyArray): int
 {
     $arrayLenght = count($anyArray);
     return rand(0, $arrayLenght - 1);
 }
+
+// function getFirstLetterUc(array $label)
+// {
+//     foreach ($label as $key => $value) {
+//         $newKey = ucfirst(($key));
+//         if ($newKey != $key) {
+//             unset($label[$key]);
+//             $label[$newKey] = $value;
+//         }
+//     }
+// }
