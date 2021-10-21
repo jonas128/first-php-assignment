2ceb5b25cc95fefca05da8c54687928624104bdc
h-emma <emmah92@hotmail.com>
2021-10-21 21:40:43 +0200

    added div classes

--- index.php
+++ index.php
@@ -5,12 +5,18 @@ require __DIR__ . '/header.php';
 ?>
 
 <main>
-    <h2>What kind off coffe do you want?</h2>
-    <p> Press the button below and we will select one for you!</p>
-    <button onClick="window.location.reload();">Select Coffee</button>
+    <div class='head2-text'>
+        <h2>What kind off coffe do you want?</h2>
+    </div>
+    <div class='main-text'>
+        <p> Press the button below and we will select one for you!</p>
+    </div>
+    <div class='button'><button onClick="window.location.reload();">Select Coffee</button></div>
     <?php $randIndex = getRandomIndex($filters) ?>
-    <img src="<?= $filters[$randIndex]['image'] ?>" alt="the label on coffee">
-    <p><?= $filters[$randIndex]['name'] ?></p>
+    <div class='coffee-choice'>
+        <p><?= $filters[$randIndex]['name'] ?></p>
+        <img src="<?= $filters[$randIndex]['image'] ?>" alt="the label on coffee">
+    </div>
 </main>
 
 
