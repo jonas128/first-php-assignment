bcae2c9874cf189c562860c80c446343558e5ab2
h-emma <emmah92@hotmail.com>
2021-10-27 21:41:17 +0200

    Change the nav to a ul and li and added all new style sheet

--- header.php
+++ header.php
@@ -10,17 +10,22 @@ require __DIR__ . '/functions.php';
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Wonderful Coffee</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/css/globel.css">
+    <link rel="stylesheet" href="/css/img.css">
+    <link rel="stylesheet" href="/css/button.css">
+    <link rel="stylesheet" href="/css/class.css">
+    <link rel="stylesheet" href="/css/@media.css">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/faviconCoffee.png" />
 </head>
 
 <body>
     <header>
-        <div class='head1-text'>
-            <h1>Welcome to Wonderful Coffee</h1>
-        </div>
+        <h1>Welcome to Wonderful Coffee</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="ourCoffee.php">Our Coffee</a></li>
+                <li><a href="reviews.php">Our reviews</a></li>
+            </ul>
+        </nav>
     </header>
-    <nav>
-        <a href="index.php">Home</a>
-        <a href="ourCoffee.php">Our Coffee</a>
-        <a href="reviews.php">Our reviews</a>
-    </nav>
