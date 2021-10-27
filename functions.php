99361eaa2da353926e1d29c686a9500a17050645
h-emma <emmah92@hotmail.com>
2021-10-27 21:38:59 +0200

    Added comments on what the functions do

--- functions.php
+++ functions.php
@@ -2,13 +2,14 @@
 
 declare(strict_types=1);
 
+//FUNCTION RETURNS A RANDOM INDEX FROM AN ARRAY
 function getRandomIndex(array $anyArray): int
 {
     $arrayLenght = count($anyArray);
     return rand(0, $arrayLenght - 1);
 }
 
-
+//FUNCTION RETURNS ONLY THE STRINGS WITH A SPECIFIC VALUE
 function getOnlyGoodReviews(array $reviews): array
 {
     $goodReviews = [];
