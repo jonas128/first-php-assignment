1d5ad50389f063bf6283e2045052ee837423021c
h-emma <emmah92@hotmail.com>
2021-10-27 21:48:45 +0200

    Remove almost all of the divs

--- index.php
+++ index.php
@@ -5,18 +5,15 @@ require __DIR__ . '/header.php';
 ?>
 
 <main>
-    <div class='head2-text'>
+    <section class="coffeeChoice">
         <h2>What kind off coffe do you want?</h2>
-    </div>
-    <div class='main-text'>
         <p> Press the button below and we will select one for you!</p>
-    </div>
-    <div class='button'><button onClick="window.location.reload();">Select Coffee</button></div>
-    <?php $randIndex = getRandomIndex($filters) ?>
-    <div class='coffee-choice'>
+
+        <div class='button'><button onClick="window.location.reload();">Select Coffee</button></div>
+        <?php $randIndex = getRandomIndex($filters) ?>
         <p><?= $filters[$randIndex]['name'] ?></p>
-        <img src="<?= $filters[$randIndex]['image'] ?>" alt="the label on coffee">
-    </div>
+        <img src="<?= $filters[$randIndex]['image'] ?>" alt="<?= $filters[$randIndex]['alt'] ?>">
+    </section>
 </main>
 
 
