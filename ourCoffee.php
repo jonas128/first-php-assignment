4214b17013054f91cd6ef85c2d03d9b2087bedc9
h-emma <emmah92@hotmail.com>
2021-10-28 19:50:58 +0200

    Added a div class

--- ourCoffee.php
+++ ourCoffee.php
@@ -6,14 +6,16 @@ require __DIR__ . '/header.php';
 <main>
     <h2>This is all of our filter coffee</h2>
     <?php foreach ($filters as $filter) : ?>
-        <p>Name: <?= $filter['name'] ?></p>
-        <p>Variety: <?= $filter['variety'] ?></p>
-        <p>Process: <?= $filter['process'] ?></p>
-        <p>Altitude: <?= $filter['altitude'] ?></p>
-        <p>Harvested: <?= $filter['harvested'] ?></p>
-        <p>Test notes: <?= $filter['testNotes'] ?></p>
-        <p>Price: <?= $filter['price'] ?></p>
-        <img src="<?= $filter['image'] ?>" alt="<?= $filter['alt'] ?>">
+        <div class="filterCoffee">
+            <p>Name: <?= $filter['name'] ?></p>
+            <p>Variety: <?= $filter['variety'] ?></p>
+            <p>Process: <?= $filter['process'] ?></p>
+            <p>Altitude: <?= $filter['altitude'] ?></p>
+            <p>Harvested: <?= $filter['harvested'] ?></p>
+            <p>Test notes: <?= $filter['testNotes'] ?></p>
+            <p>Price: <?= $filter['price'] ?></p>
+            <img src="<?= $filter['image'] ?>" alt="<?= $filter['alt'] ?>">
+        </div>
     <?php endforeach; ?>
 </main>
 
