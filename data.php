2d69e0090f5856e46efce4428c571bc83786d404
h-emma <emmah92@hotmail.com>
2021-10-28 14:39:50 +0200

    Added alt text to the images

--- data.php
+++ data.php
@@ -13,7 +13,7 @@ $filters = [
         'testNotes' => 'Juicy and sweet with floral notes of strawberry lemonade and peach',
         'price' => 399,
         'image' => 'images\oreti.jpeg',
-        'alt' => '',
+        'alt' => 'An image with a lot of objects. A ceramic hen and rhino. Two glass bowls with coffee in and a glass jug with some coffee in',
     ],
     [
         'name' => 'Montero Family',
@@ -24,7 +24,7 @@ $filters = [
         'testNotes' => 'Structured and creamy with notes of chocolate biscuit and ripe plum',
         'price' => 259,
         'image' => 'images\montero.png',
-        'alt' => '',
+        'alt' => 'An image with a lot of objects. A biscuit chocolate. An eaten apple. A pink plastic hand. Some objects in white proslin',
     ],
     [
         'name' => 'Gotiti',
@@ -35,7 +35,7 @@ $filters = [
         'testNotes' => 'Jammy and elegant with notes of elderflower and red currant lemonade',
         'price' => 259,
         'image' => 'images\gotiti.jpeg',
-        'alt' => '',
+        'alt' => 'An image with a lot of objects. A glass with red liquid and flowers in. A glass with flames on. A cone, purple plastic grape and a yellow lemon',
     ],
     [
         'name' => 'Bekele Legie',
@@ -46,7 +46,7 @@ $filters = [
         'testNotes' => 'Juicy and sweet with notes of ripe grapes and Earl Grey tea',
         'price' => 259,
         'image' => 'images\bekeleLegie.png',
-        'alt' => '',
+        'alt' => 'An image with a lot of objects. A wooden horse. Purple grapes, purple plums, some without skin. A red shoelace. Purple and pink flowers. A sea lion in porcelain',
     ],
     [
         'name' => 'Sandra Milena Mora',
@@ -57,7 +57,7 @@ $filters = [
         'testNotes' => 'Layered and sweet with notes of red apple and milk chocolate',
         'price' => 399,
         'image' => 'images\sandraMilenaMora.jpeg',
-        'alt' => '',
+        'alt' => 'An image with a lot of objects. A brown light. A back of a dog from the side. A piece of apple. Four pieces of chocolate in the form of three seashells and a fish',
     ],
 ];
 
