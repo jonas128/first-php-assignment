1c63731b3251172e4cdaf90177edef3282262519
h-emma <emmah92@hotmail.com>
2021-10-28 14:20:00 +0200

    Deleted a div allFilterCoffee

--- ourCoffee.php
+++ ourCoffee.php
@@ -5,19 +5,16 @@ require __DIR__ . '/header.php';
 ?>
 <main>
     <h2>This is all of our filter coffee</h2>
-    <div class='allFilterCoffee'>
-        <?php foreach ($filters as $filter) : ?>
-            <p>Name: <?= $filter['name'] ?></p>
-            <p>Variety: <?= $filter['variety'] ?></p>
-            <p>Process: <?= $filter['process'] ?></p>
-            <p>Altitude: <?= $filter['altitude'] ?></p>
-            <p>Harvested: <?= $filter['harvested'] ?></p>
-            <p>Test notes: <?= $filter['testNotes'] ?></p>
-            <p>Price: <?= $filter['price'] ?></p>
-            <img src="<?= $filter['image'] ?>" alt="<?= $filter['alt'] ?>">
-        <?php endforeach; ?>
-    </div>
-
+    <?php foreach ($filters as $filter) : ?>
+        <p>Name: <?= $filter['name'] ?></p>
+        <p>Variety: <?= $filter['variety'] ?></p>
+        <p>Process: <?= $filter['process'] ?></p>
+        <p>Altitude: <?= $filter['altitude'] ?></p>
+        <p>Harvested: <?= $filter['harvested'] ?></p>
+        <p>Test notes: <?= $filter['testNotes'] ?></p>
+        <p>Price: <?= $filter['price'] ?></p>
+        <img src="<?= $filter['image'] ?>" alt="<?= $filter['alt'] ?>">
+    <?php endforeach; ?>
 </main>
 
 <?php
